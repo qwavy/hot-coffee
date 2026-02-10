2d3c069ee65196947a84604d4592687527187797
nualdaber <nurss.aldb@gmail.com>
2026-02-10 15:21:52 +0500

    feat: implement GET orders

--- internal/dal/order_repository.go
+++ internal/dal/order_repository.go
@@ -1,9 +1,33 @@
 package dal
 
+import (
+	"encoding/json"
+	"hot-coffee/models"
+	"os"
+)
+
 type OrderRepository struct {
 	filePath string
 }
 
 func NewOrderRepository(filePath string) *OrderRepository {
 	return &OrderRepository{filePath: filePath}
+}
+func (r *OrderRepository) listO() ([]models.OrderItem, error) {
+	data, err := os.ReadFile(r.filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var orderItem []models.OrderItem
+
+	err = json.Unmarshal(data, &orderItem)
+	if err != nil {
+		return nil, err
+	}
+
+	return orderItem, nil
+}
+func (r *OrderRepository) GetAll() ([]models.OrderItem, error) {
+	return r.listO()
 }
--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -1,6 +1,9 @@
 package service
 
-import "hot-coffee/internal/dal"
+import (
+	"hot-coffee/internal/dal"
+	"hot-coffee/models"
+)
 
 type OrderService struct {
 	OrderRepository *dal.OrderRepository
@@ -8,4 +11,14 @@ type OrderService struct {
 
 func NewOrderService(orderRepository *dal.OrderRepository) *OrderService {
 	return &OrderService{OrderRepository: orderRepository}
+}
+
+func (s *OrderService) GetAll() ([]models.OrderItem, error) {
+	orderItem, err := s.OrderRepository.GetAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return orderItem, nil
 }
